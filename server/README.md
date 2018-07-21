f25204de8da1bcaae767d916090d428f07d99539
cocodrips <cocodrips@gmail.com>
2018-07-21 15:01:07 +0900

    Change web app port.

--- server/README.md
+++ server/README.md
@@ -1,3 +1,13 @@
 # interface
 
 `/add` (post)
+
+|param|description|
+|:---|:---|
+|uname|人の名前|
+|ainame|AIの名前|
+|score|スコア|
+|problem|問題番号|
+|nbt|nbtファイル
+
+## 
--- server/web.py
+++ server/web.py
@@ -8,17 +8,17 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+
 @app.route("/add", methods=['POST'])
 def add_data():
-    print (request.data)
+    print(request.data)
     f = request.files.get('nbt')
     return "OK"
-    
 
 
 if __name__ == '__main__':
     app.run(
         host=os.environ.get('FLASK_HOST', '0.0.0.0'),
-        port=int(os.environ.get('FLASK_PORT', 5000)),
+        port=int(os.environ.get('FLASK_PORT', 5050)),
         debug=bool(os.environ.get('FLASK_DEBUG', 1))
     )
