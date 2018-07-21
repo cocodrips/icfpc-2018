a1053543f5d7508519864dbf30b19b5ef87a25e4
Tomoyuki Nakatsuka <nakatsuka@liquidinc.asia>
2018-07-21 16:32:35 +0900

    ファイルの存在チェックを入れた

--- simulator/hoge.js
+++ simulator/hoge.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 const puppeteer = require('puppeteer');
 const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
@@ -8,7 +9,20 @@ if (process.argv.length < 4) {
 }
 
 const modelFile = process.argv[2];
+try {
+  fs.statSync(modelFile);
+} catch (e) {
+  console.error('モデルファイルが存在しません');
+  process.exit(1);
+}
+
 const traceFile = process.argv[3];
+try {
+  fs.statSync(traceFile);
+} catch (e) {
+  console.error('トレースファイルが存在しません');
+  process.exit(1);
+}
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -30,6 +44,7 @@ const traceFile = process.argv[3];
   let content = '';
   while (true) {
     content = await page.$eval('#stdout', item => item.textContent);
+    console.log(content);
     if (content.startsWith('Success::')) {
       break;
     }
