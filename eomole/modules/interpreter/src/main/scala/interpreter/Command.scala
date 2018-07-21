f1e653fded737648e6801274cba4642bdffc40c4
Hiroshi Yamaguchi <sanberg12@gmail.com>
2018-07-21 19:00:12 +0900

    interpreter書いた。けどenergyの値が合わない

--- eomole/modules/interpreter/src/main/scala/interpreter/Command.scala
+++ eomole/modules/interpreter/src/main/scala/interpreter/Command.scala
@@ -20,6 +20,8 @@ case class LLD(axis: Axis, d: Int) {
   def dy: Int = if (axis == Axis.Y) d else 0
 
   def dz: Int = if (axis == Axis.Z) d else 0
+
+  def mlen: Int = Math.abs(d)
 }
 
 case class SLD(axis: Axis, d: Int) {
@@ -30,14 +32,18 @@ case class SLD(axis: Axis, d: Int) {
   def dy: Int = if (axis == Axis.Y) d else 0
 
   def dz: Int = if (axis == Axis.Z) d else 0
+
+  def mlen: Int = Math.abs(d)
 }
 
 case class ND(dx: Int, dy: Int, dz: Int) {
   require(-1 <= dx && dx <= 1, s"$dx $dy $dz")
   require(-1 <= dy && dy <= 1, s"$dx $dy $dz")
   require(-1 <= dz && dz <= 1, s"$dx $dy $dz")
-  require(Math.abs(dx) + Math.abs(dy) + Math.abs(dz) >= 1, s"$dx $dy $dz")
-  require(Math.abs(dx) + Math.abs(dy) + Math.abs(dz) <= 2, s"$dx $dy $dz")
+  require(mlen >= 1, s"$dx $dy $dz")
+  require(mlen <= 2, s"$dx $dy $dz")
+
+  def mlen: Int = Math.abs(dx) + Math.abs(dy) + Math.abs(dz)
 }
 
 sealed trait Command
