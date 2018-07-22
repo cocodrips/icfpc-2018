6a0e7ba11cfe3add1a2b0a464f475340b49be92e
cocodrips <cocodrips@gmail.com>
2018-07-22 16:59:18 +0900

    Update post script.

--- server/post_nbt.sh
+++ server/post_nbt.sh
@@ -2,7 +2,8 @@
 
 ai="AI_NAME"
 user="USER_NAME"
+type_="LA/FA/FD/FR"
 for i in `seq 1 186`; do
     filename=$(printf "%03d" $i)
-    curl -X POST http://54.244.193.90:5050/add -F "nbt=@../data/dfltTracesL/LA${filename}.nbt" -F user=${user} -F ai=${ai} -F problem=$i
+    curl -X POST http://54.244.193.90:5050/add -F "nbt=@../data/dfltTracesL/LA${filename}.nbt" -F user=${user} -F ai=${ai} -F problem=$i -F type=${type_}
 done
