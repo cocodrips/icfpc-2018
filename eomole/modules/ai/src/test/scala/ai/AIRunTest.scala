aeddf0737b067820a80ae8667c1deb82cfb29e5d
Hiroshi Yamaguchi <sanberg12@gmail.com>
2018-07-23 03:56:55 +0900

    生成を改善

--- eomole/modules/ai/src/test/scala/ai/AIRunTest.scala
+++ eomole/modules/ai/src/test/scala/ai/AIRunTest.scala
@@ -2,7 +2,7 @@ package ai
 
 import java.nio.file.{Files, Paths}
 
-import interpreter.{Interpreter, Model, TraceEncoder}
+import interpreter._
 import org.scalatest.FunSuite
 
 class AIRunTest extends FunSuite {
@@ -13,29 +13,17 @@ class AIRunTest extends FunSuite {
     i <- 1 to 186
     ai <- ais
   } {
-    test("%sはL構築ができる LA%03d".format(ai.name, i)) {
-      val target = Model.decode(Files.readAllBytes(Paths.get("../data/problems/LA%03d_tgt.mdl".format(i))))
-      val source = Model.empty(target.R)
-      val trace = ai.solve(source, target)
-      Files.write(Paths.get("../data/%s/LA%03d.nbt".format(ai.name, i)), TraceEncoder.encode(trace).toArray)
-      val result = Interpreter.execute(source, trace, verbose = false)
-      println(i, result.energy)
-      require(target.bitset == result.matrix.bitset, "Expected ans printed are not the same.")
-    }
-  }
-
-  for {
-    i <- 1 to 186
-    ai <- ais
-  } {
     test("%sはF構築ができる FA%03d".format(ai.name, i)) {
-      val target = Model.decode(Files.readAllBytes(Paths.get("../data/problems/FA%03d_tgt.mdl".format(i))))
-      val source = Model.empty(target.R)
-      val trace = ai.solve(source, target)
+      val targetL = Model.decode(Files.readAllBytes(Paths.get("../data/problems/LA%03d_tgt.mdl".format(i))))
+      val targetF = Model.decode(Files.readAllBytes(Paths.get("../data/problems/FA%03d_tgt.mdl".format(i))))
+      val source = Model.empty(targetF.R)
+      val trace = ai.solve(source, targetF)
+      Files.write(Paths.get("../data/%s/LA%03d.nbt".format(ai.name, i)), TraceEncoder.encode(trace).toArray) // 一緒なので同時に
       Files.write(Paths.get("../data/%s/FA%03d.nbt".format(ai.name, i)), TraceEncoder.encode(trace).toArray)
       val result = Interpreter.execute(source, trace, verbose = false)
       println(i, result.energy)
-      require(target.bitset == result.matrix.bitset, "Expected ans printed are not the same.")
+      require(targetL.bitset == result.matrix.bitset, "Expected ans printed are not the same.")
+      require(targetF.bitset == result.matrix.bitset, "Expected ans printed are not the same.")
     }
   }
 
@@ -46,7 +34,7 @@ class AIRunTest extends FunSuite {
     test("%sはF破壊ができる FD%03d".format(ai.name, i)) {
       val source = Model.decode(Files.readAllBytes(Paths.get("../data/problems/FD%03d_src.mdl".format(i))))
       val target = Model.empty(source.R)
-      val trace = ai.solve(source, target)
+      val trace = Reverser.reverse(TraceDecoder.decode(Files.readAllBytes(Paths.get("../data/dfltTraces/LA%03d.nbt".format(i)))))
       Files.write(Paths.get("../data/%s/FD%03d.nbt".format(ai.name, i)), TraceEncoder.encode(trace).toArray)
       val result = Interpreter.execute(source, trace, verbose = false)
       println(i, result.energy)
---
+++ eomole/modules/interpreter/src/main/scala/interpreter/Reverser.scala
@@ -0,0 +1,20 @@
+package interpreter
+
+object Reverser {
+
+  def reverse(commands: Seq[Command]): Seq[Command] =
+    commands.reverse match {
+      case Command.Halt +: tail => tail.map(trans) :+ Command.Halt
+      case seq => seq.map(trans)
+    }
+
+  private def trans(c: Command): Command = c match {
+    case Command.SMove(lld) => Command.SMove(LLD(lld.axis, -lld.d))
+    case Command.LMove(sld1, sld2) => Command.LMove(SLD(sld1.axis, -sld1.d), SLD(sld2.axis, -sld2.d))
+    case Command.Fill(nd) => Command.Void(nd)
+    case Command.Void(nd) => Command.Fill(nd)
+    case Command.GFill(nd, fd) => Command.GVoid(nd, fd)
+    case Command.GVoid(nd, fd) => Command.GFill(nd, fd)
+    case c => c // TODO: FissionとFusionのreverseに対応する
+  }
+}
