ec4e849bebfff891f740caa473dd147fdd738dc2
Hiroshi Yamaguchi <sanberg12@gmail.com>
2018-07-23 02:15:40 +0900

    Flip遅延と不必要なYを削る処理を入れたAI追加

--- eomole/modules/ai/src/test/scala/ai/AICompareTest.scala
+++ eomole/modules/ai/src/test/scala/ai/AICompareTest.scala
@@ -7,10 +7,10 @@ import org.scalatest.FunSuite
 
 class AICompareTest extends FunSuite {
 
-  val ais: Seq[AI] = Seq(NearestAI)
+  val ais: Seq[AI] = Seq(NearestLowAI)
 
   for {
-    i <- 1 to 186
+    i <- 1 to 20 //186
     ai <- ais
   } {
     test("%sはL構築がデフォルトより優れている LA%03d".format(ai.name, i)) {
@@ -21,12 +21,13 @@ class AICompareTest extends FunSuite {
       val default = TraceDecoder.decode(Files.readAllBytes(Paths.get("../data/dfltTraces/LA%03d.nbt".format(i))))
       val defaultResult = Interpreter.execute(source, default, verbose = false)
       println(i, result.energy, defaultResult.energy, 100 * result.energy / defaultResult.energy + "%")
+      require(target.bitset == result.matrix.bitset, "Expected ans printed are not the same.")
       require(result.energy < defaultResult.energy, "Lose!")
     }
   }
 
   for {
-    i <- 1 to 186
+    i <- 1 to 20 //186
     ai <- ais
   } {
     test("%sはF構築がデフォルトより優れている FA%03d".format(ai.name, i)) {
@@ -37,12 +38,13 @@ class AICompareTest extends FunSuite {
       val default = TraceDecoder.decode(Files.readAllBytes(Paths.get("../data/dfltTraces/FA%03d.nbt".format(i))))
       val defaultResult = Interpreter.execute(source, default, verbose = false)
       println(i, result.energy, defaultResult.energy, 100 * result.energy / defaultResult.energy + "%")
+      require(target.bitset == result.matrix.bitset, "Expected ans printed are not the same.")
       require(result.energy < defaultResult.energy, "Lose!")
     }
   }
 
   for {
-    i <- 1 to 186
+    i <- 1 to 20 //186
     ai <- ais
   } {
     test("%sはF破壊がデフォルトより優れている FD%03d".format(ai.name, i)) {
@@ -53,12 +55,13 @@ class AICompareTest extends FunSuite {
       val default = TraceDecoder.decode(Files.readAllBytes(Paths.get("../data/dfltTraces/FD%03d.nbt".format(i))))
       val defaultResult = Interpreter.execute(source, default, verbose = false)
       println(i, result.energy, defaultResult.energy, 100 * result.energy / defaultResult.energy + "%")
+      require(target.bitset == result.matrix.bitset, "Expected ans printed are not the same.")
       require(result.energy < defaultResult.energy, "Lose!")
     }
   }
 
   for {
-    i <- 1 to 115
+    i <- 1 to 20 //115
     ai <- ais
   } {
     test("%sはF再構築がデフォルトより優れている FR%03d".format(ai.name, i)) {
@@ -69,6 +72,7 @@ class AICompareTest extends FunSuite {
       val default = TraceDecoder.decode(Files.readAllBytes(Paths.get("../data/dfltTraces/FR%03d.nbt".format(i))))
       val defaultResult = Interpreter.execute(source, default, verbose = false)
       println(i, result.energy, defaultResult.energy, 100 * result.energy / defaultResult.energy + "%")
+      require(target.bitset == result.matrix.bitset, "Expected ans printed are not the same.")
       require(result.energy < defaultResult.energy, "Lose!")
     }
   }
