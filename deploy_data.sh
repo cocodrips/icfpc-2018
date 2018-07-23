3a79679d4f8550e992792cdd93ba1020f8a5439d
Tomoyuki Nakatsuka <nakatsuka@liquidinc.asia>
2018-07-23 11:18:18 +0900

    simulator下にもdataを展開する

--- deploy_data.sh
+++ deploy_data.sh
@@ -6,8 +6,13 @@ cp icfpcontest2018.github.io/assets/problemsL.zip data/
 cp icfpcontest2018.github.io/assets/problemsF.zip data/
 
 mkdir -p data/dfltTraces data/problems
+mkdir -p simulator/data/dfltTraces simulator/data/problems
 unzip data/dfltTracesL.zip -d data/dfltTraces
 unzip data/dfltTracesF.zip -d data/dfltTraces
 unzip data/problemsL.zip -d data/problems
 unzip data/problemsF.zip -d data/problems
+unzip data/dfltTracesL.zip -d simulator/data/dfltTraces
+unzip data/dfltTracesF.zip -d simulator/data/dfltTraces
+unzip data/problemsL.zip -d simulator/data/problems
+unzip data/problemsF.zip -d simulator/data/problems
 rm data/*.zip
